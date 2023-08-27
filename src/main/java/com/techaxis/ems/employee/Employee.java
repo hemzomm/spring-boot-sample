fa428796311fad63efb21fe75836469cbff9097b
hemz <hemzomm@gmail.com>
2023-08-27 15:52:07 +0545

    Adding package structure to project for Spring Boot Application

---
+++ src/main/java/com/techaxis/ems/employee/Employee.java
@@ -0,0 +1,14 @@
+package com.techaxis.ems.employee;
+
+import java.time.LocalDate;
+
+public class Employee {
+    private Long id;
+    private String name;
+    private LocalDate dob;
+    private Integer age;
+    private String address;
+    private String email;
+    private String phone;
+
+}
